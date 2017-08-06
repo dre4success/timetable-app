8c780c70bf4385d5967666a555c63c824b85ccfd
Damilare Joseph <dre4success@live.com>
2017-08-06 16:30:08 +0100

    Registering models

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+
+dotenv.config({ path: 'variables.env'});
+
+// connect to database
+mongoose.connect(process.env.DATABASE, {
+	useMongoClient: true
+})
+
+// tell mongoose to use es6 promise since it's own is deprecated
+mongoose.Promise = global.Promise;
+
+// handle bad connections
+mongoose.connection.on('error', (err) => {
+	console.log(`${err.message}`);
+})
+
+// import all models
+require('./models/Course');
+require('./models/User');
+
 const app = require('./app');
 
 app.listen(3000, () => {
