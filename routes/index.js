2cb753b0cd1f24859ac4e091ed81ae04b24f1cdc
Damilare Joseph <dre4success@live.com>
2017-08-06 16:32:33 +0100

    mounting more routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,28 @@
 const express = require('express');
 const router = express.Router();
-const testController = require('../controllers/testing');
+const courseController = require('../controllers/courseController');
+const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 
-router.get('/', testController.homepage)
+const { catchErrors } = require('../handlers/errorHandlers');
+
+router.get('/', courseController.getHome);
+
+router.get('/add', authController.isLoggedIn, courseController.addCourse);
+router.post('/add', catchErrors(courseController.saveCourse));
+
+router.get('/view', catchErrors(courseController.viewCourse));
+
+router.get('/register', userController.registerForm);
+router.get('/login', userController.loginForm);
+router.post('/login', authController.login);
+
+router.post('/register', 
+	userController.validateRegister,
+	userController.register,
+	authController.login);
+
+router.get('/logout', authController.logout);
+
 
 module.exports = router;
