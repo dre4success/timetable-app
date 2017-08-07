41fe7280a6b9303f69288d1ac79f5a1d188ebb15
Damilare Joseph <dre4success@live.com>
2017-08-07 11:07:27 +0100

    making controllers for updating and deleting

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -12,16 +12,38 @@ exports.addCourse = (req, res) => {
 exports.saveCourse = async (req, res) => {
 	const course = new Course(req.body);
 	await course.save()
-	res.json(course)
+	req.flash('success', `Successfully added ${course.courseName} to timetable`)
+	res.redirect('/')
 	//res.send("IT works")
 }
 
 exports.viewCourse = async(req, res) => {
-	// const view = await Course.find()
-	 const day = await Course.getCourseByday()
-	 // const timePromise = Course.find({days: dayPromise._id})
-	 // const timePromise = Course.getCourseByTime()
-	 // const  = await Promise.all([dayPromise, timePromise]);
-	  // res.render('view', {title: 'View Table', daytime})
-	 res.json(day)
+	  const day = req.params.day;
+	  const daytimePromise = Course.getCourse()
+	  const coursePromise = Course.find({ days: day});
+
+	  const [days, course] = await Promise.all([daytimePromise, coursePromise]);
+	  
+	  res.render('view', {title: 'View Table', days, day, course})
+	    
+	  
+}
+exports.editCourse = async(req, res) => {
+	const course = await Course.findOne({_id: req.params.id});
+	res.render('addCourse', {title: `Edit ${course.courseName}`, course})
+}  
+
+exports.updateCourse = async(req, res) => {
+	const course = await Course.findOneAndUpdate({ _id: req.params.id}, req.body, {
+		new: true, // return the new store instead of the old one
+		runValidators: true
+	}).exec();
+	req.flash('success', `Successfully Updated ${course.courseName}`);
+	res.redirect(`/view/${course._id}/edit`)
+}
+
+exports.deleteCourse = async (req, res) => {
+	await Course.deleteOne({ _id: req.params.id})
+	req.flash('success', 'Successfully deleted course');
+	res.redirect('/view');
 }
