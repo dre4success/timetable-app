9a996ebd853b707fcb63cb33e37406a191c55f6a
Damilare Joseph <dre4success@live.com>
2017-08-07 11:12:33 +0100

    adding more routes

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,19 @@ const authController = require('../controllers/authController');
 
 const { catchErrors } = require('../handlers/errorHandlers');
 
-router.get('/', courseController.getHome);
+router.get('/', courseController.viewCourse);
 
 router.get('/add', authController.isLoggedIn, courseController.addCourse);
 router.post('/add', catchErrors(courseController.saveCourse));
 
 router.get('/view', catchErrors(courseController.viewCourse));
+router.get('/view/:day', catchErrors(courseController.viewCourse));
+
+router.get('/view/:id/edit', catchErrors(courseController.editCourse));
+router.post('/add/:id', catchErrors(courseController.updateCourse));
+
+router.get('/view/:id/delete', catchErrors(courseController.deleteCourse));
+
 
 router.get('/register', userController.registerForm);
 router.get('/login', userController.loginForm);
