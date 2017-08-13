320a246c7c958c909dd37a73e322f17df68ce2d2
Damilare Joseph <dre4success@live.com>
2017-08-13 21:59:08 +0100

    correcting a defect

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const Course = mongoose.model('Course');
+const http = require('../app');
+const io = require('socket.io')(http);
 
 exports.getHome = (req, res) => {
 	res.render('layout', {title: 'Dashboard'})
@@ -15,12 +17,21 @@ exports.saveCourse = async (req, res) => {
 	temp.daytime = [];
 	const times = req.body.daytime.time;
 	const days = req.body.daytime.day;
+	// console.log(times, days);
+	if(Array.isArray(times)) {
 	times.forEach((time, index) => {
 		temp.daytime.push({time, 'day': days[index]});
 	})
-	const course = new Course(temp);
+	var course = new Course(temp);
 	await course.save()
-	// console.log(temp);
+} else {
+
+	var course = new Course(req.body)
+	await course.save()
+}
+	/*const course = new Course(temp);
+	await course.save()*/
+
 	req.flash('success', `Successfully added ${course.courseName} to timetable`)
 	res.redirect('/')
 
@@ -36,10 +47,16 @@ exports.viewApi = async(req, res) => {
 
 	const course = await Course.find()
 	res.json(course)
+	io.on('connection', (socket) => {
+		console.log("new course added")
+	socket.on('update', (course) => {
+		io.emit('update', course);
+		})
+	})
 }
 exports.editCourse = async(req, res) => {
 	const course = await Course.findOne({_id: req.params.id});
-	res.render('addCourse', {title: `Edit ${course.courseName}`, course})
+	res.render('editCourse', {title: `Edit ${course.courseName}`, course})
 }  
 
 exports.updateCourse = async(req, res) => {
@@ -48,13 +65,20 @@ exports.updateCourse = async(req, res) => {
 	update.daytime = [];
 	const times = req.body.daytime.time;
 	const days = req.body.daytime.day;
+	if(Array.isArray(days)) {
 	days.forEach((day, i) => {
 		update.daytime.push({'time':times[i], day})
 	})
-	const course = await Course.findOneAndUpdate({ _id: req.params.id}, update, {
+	var course = await Course.findOneAndUpdate({ _id: req.params.id}, update, {
 		new: true, // return the new store instead of the old one
 		runValidators: true
 	}).exec();
+} else {
+	var course = await Course.findOneAndUpdate({ _id: req.params.id}, req.body, {
+		new: true, // return the new store instead of the old one
+		runValidators: true
+	}).exec();
+}
 	req.flash('success', `Successfully Updated ${course.courseName}`);
 	res.redirect(`/view/${course._id}/edit`)
 }
